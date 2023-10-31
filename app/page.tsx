7bfab1add9f5620a4eedee1ace21f6a91f96db6b
Bogdan <1298bogda@gmail.com>
2023-10-31 16:38:48 +0300

    переименована кнопка в button

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     className="px-3 py-1.5 text-sm text-white duration-150 bg-indigo-600 rounded-lg hover:bg-indigo-700 active:shadow-lg"
 >
 
-    Button
+    Кнопка
 </button>
 
 <button
